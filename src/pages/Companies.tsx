b46632393671325134d42905b704d458f0716f2f
MaksymBabii <maksymbabiiwork@gmail.com>
2021-12-18 21:53:01 +0200

    rollback react-script to version 4.0.3, added dependencies mui

--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import {Button} from "@mui/material";
 
 interface CompaniesProps {}
 
 export const Companies: React.FC<CompaniesProps> = () => {
-  return <div>hi</div>;
+  return <div>hi <Button variant="contained">Text</Button></div>;
 };
